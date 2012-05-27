c0eceb82ebce5cc48d42999dfb8166fca23ef6ba
Haehnchen <espendiller@gmx.de>
2012-05-27 11:00:31 +0200

    longlife facebook auth on server side

--- extra/fb.php
+++ extra/fb.php
@@ -1,3 +1,31 @@
+<?php
+if(isset($_GET['access_token'])) {
+
+  // more validate here
+  $access_token = $_GET['access_token'];
+  if (!preg_match("/^[A-Za-z0-9]+$/", $access_token)) {
+    echo 'error';
+    exit;
+  }
+  
+  $client_id = '440490789296919';
+  $client_secret = 'ENTER_YOUR_KEY_HERE';
+  $ex_url = 'https://graph.facebook.com/oauth/access_token?client_id=' . $client_id . '&client_secret=' . $client_secret . '&grant_type=fb_exchange_token&fb_exchange_token=%s';
+  
+
+  $response = @file_get_contents(sprintf($ex_url, $access_token));
+  parse_str($response, $out);
+  
+  if(isset($out['expires'])) {
+    $out['expires_in'] = $out['expires'];
+  }  
+  
+  echo json_encode($out);
+  
+  exit;
+}
+?>
+
 <!DOCTYPE html PUBLIC 
   "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -9,60 +37,72 @@
     </head>
     <script>
     
-function getParameters() {
-  var searchString = window.location.hash.substring(1)
-  var params = searchString.split("&")
-  var hash = {}
 
-  for (var i = 0; i < params.length; i++) {
-    var val = params[i].split("=");
-    hash[unescape(val[0])] = unescape(val[1]);
-  }
-  return hash;
-}
-
-  function submittobox() {
-    form_data = form.serialize();
-    form_url = form.attr('action')
-    //console.log(url);
-    //$.post(url, function(data) {
-     // $('.result').html(data);
-    //})
-    
-    $.ajax({
-      dataType: 'jsonp',
-      jsonp: 'jsonp_callback',
-      url: form_url,
-      data: form_data,
-      success: function (data) {
-        //obj.html(data.text);
-        form.find('#result').html(data.msg);
-      }
-    }).fail(function() { form.find('#result').html('error'); });    
-    
-
-  }
-    
     
 $(document).ready(function() {
-    //alert(document.getElementById('test').contentWindow.location.href);
-    //console.log();
-    form = $('#box_form');
-    parms = getParameters();
-    $.each(parms, function(key, value) { 
-      form.find('#' + key).val(value);
-    });   
+
+  function getParameters() {
+    var searchString = window.location.hash.substring(1)
+    var params = searchString.split("&")
+    var hash = {}
+
+    for (var i = 0; i < params.length; i++) {
+      var val = params[i].split("=");
+      hash[unescape(val[0])] = unescape(val[1]);
+    }
+    return hash;
+  }
+
+    function submittobox() {
+      form_data = form.serialize();
+      form_url = form.attr('action')
+
+      $.ajax({
+        dataType: 'jsonp',
+        jsonp: 'jsonp_callback',
+        url: form_url,
+        data: form_data,
+        success: function (data) {
+          setMsg(data.msg);
+        }
+      }).fail(function() { setMsg('error'); });    
+      
+
+    }
+    
+    function longlife() {
+      $.getJSON('/fb.php', form.serialize(), function(data) {
+        FillForm(data)
+        submittobox()
+      });
+    }
+    
+    function setMsg(msg) {
+      form.find('#result').html(msg);
+    }
+      
+    function FillForm(items) {
+      $.each(items, function(key, value) { 
+        form.find('#' + key).val(value);
+      });    
+    }
+
+
+    var form = $('#box_form');
+    FillForm(getParameters())
 
     submittobox()
     
     form.submit(function(event) {
       event.preventDefault() 
-      submittobox()
     });
-  
-
-
     
+    $("#fb_longlife").click(function(event) {
+      event.preventDefault() 
+      setMsg('requesting...')
+      longlife();
+    });    
+
 });      
     </script>
     <body>
@@ -70,7 +110,7 @@ $(document).ready(function() {
 <form action="<?php print $_GET['local']; ?>" method="post" id='box_form'>
 
     <div>
-      <p>Submit box on on: <?php print $_GET['local']; ?></p>
+      <p>Submit to box on: <?php print $_GET['local']; ?></p>
     </div>
 
     <div>
@@ -82,6 +122,7 @@ $(document).ready(function() {
          <input type="text" name="expires_in" id="expires_in" value="" tabindex="1" />
     </div>
 	    <input type="submit" value="Resubmit to box" />
+      <input type="submit" id="fb_longlife" value="Request Longlive" />
     </div>
     
     <p>Your box returned: <b><span id="result"></span></b></p>
